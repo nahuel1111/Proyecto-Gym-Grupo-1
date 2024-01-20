d66309f4751ecb8b87e4b9c9dc265925933a8021
nahuel1111 <ricardo-nahuel@hotmail.com>
2024-01-20 09:37:43 -0300

    se agrega diseño al nav y un poco al home

--- src/routes/RoutesViews.jsx
+++ src/routes/RoutesViews.jsx
@@ -1,11 +1,19 @@
 
+import { Routes, Route } from 'react-router-dom'
+import Footerx from '../components/Footerx'
+import Navbarx from '../components/Navbarx'
+import HomePage from '../pages/HomePage'
+
 const RoutesViews = () => {
   return (
     <>
 
+    <Navbarx/>
     <Routes>
- 
+    <Route path='/' element={<HomePage/>} />
+  
     </Routes>
+    
 
   </>  )
 }

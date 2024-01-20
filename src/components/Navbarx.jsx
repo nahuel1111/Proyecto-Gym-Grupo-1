8d1419ccb998567ff5249880b39120057d5787cd
nahuel1111 <ricardo-nahuel@hotmail.com>
2024-01-19 22:04:46 -0300

    se cambia algunas cosas

--- src/components/Navbarx.jsx
+++ src/components/Navbarx.jsx
@@ -13,7 +13,7 @@ const Navbarx = () => {
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">  
             <Nav.Link href="#home">Inicio</Nav.Link> 
-            <Nav.Link href="#link">Sobre nosotros</Nav.Link>  
+            <Nav.Link href="#link">link</Nav.Link>  
             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">

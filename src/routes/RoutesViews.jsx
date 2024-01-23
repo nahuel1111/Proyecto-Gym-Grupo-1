ae38574db38a01c60b58eee31baefe4e190a2c17
Cesar Hermida <137806786+Sexcsar@users.noreply.github.com>
2024-01-23 18:27:15 -0300

    Se crea la ProductPage

--- src/routes/RoutesViews.jsx
+++ src/routes/RoutesViews.jsx
@@ -1,8 +1,8 @@
-
 import { Routes, Route } from 'react-router-dom'
 import Footerx from '../components/Footerx'
 import Navbarx from '../components/Navbarx'
 import HomePage from '../pages/HomePage'
+import ProductPage from '../pages/ProductPage'
 
 const RoutesViews = () => {
   return (
@@ -11,7 +11,7 @@ const RoutesViews = () => {
     <Navbarx/>
     <Routes>
     <Route path='/' element={<HomePage/>} />
-  
+    <Route path='/Productos' element={<ProductPage/>}/>
     </Routes>
     
 

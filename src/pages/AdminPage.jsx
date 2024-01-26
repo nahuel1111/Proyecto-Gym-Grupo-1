9477c810942a0e70f7aa4a535aa98c4e3cc9d374
nahuel1111 <ricardo-nahuel@hotmail.com>
2024-01-26 19:01:14 -0300

    se crea toda la estructura de admin

---
+++ src/pages/AdminPage.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Table from 'react-bootstrap/Table';
+
+import '../css/AdminPage.css'
+const AdminPage = () => {
+  return (
+    <>
+    <Container>
+    <h2>Administrador</h2>
+    </Container>
+   
+    </>
+  )
+}
+
+export default AdminPage
--- src/routes/RoutesViews.jsx
+++ src/routes/RoutesViews.jsx
@@ -7,6 +7,11 @@ import PlansPage from '../pages/PlansPage'
 import Footerx from '../components/Footerx'
 import LoginPage from '../pages/LoginPage'
 import RegisterPage from '../pages/RegisterPage'
+import AdminPage from '../pages/AdminPage'
+import AdminProductsPage from '../pages/AdminProductsPage'
+import AdminClassPage from '../pages/AdminClassPage'
+import AdminUserPage from '../pages/AdminUserPage'
+import AdminComment from '../pages/AdminComment'
 
 
 const RoutesViews = () => {
@@ -21,6 +26,11 @@ const RoutesViews = () => {
     <Route path='/PlansPage' element={<PlansPage/>}/>
     <Route path='/LoginPage' element={<LoginPage/>}/>
     <Route path='/RegisterPage' element={<RegisterPage/>}/>
+    <Route path='/AdminPage' element={<AdminPage/>}/>
+    <Route path='/AdminProduct' element={<AdminProductsPage/>}/>
+    <Route path='/AdminProductClass' element={<AdminClassPage/>}/>
+    <Route path='/AdminUser' element={<AdminUserPage/>}/>
+    <Route path='/AdminComment' element={<AdminComment/>}/>
 
     </Routes>
     <Footerx />

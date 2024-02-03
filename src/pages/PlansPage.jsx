d9bcb9849b322bbebaca3a201ec3dd9b3dfeb8b2
Cesar Hermida <137806786+Sexcsar@users.noreply.github.com>
2024-02-03 17:22:57 -0300

    se realizan cambios en el proyecto

--- src/pages/PlansPage.jsx
+++ src/pages/PlansPage.jsx
@@ -4,20 +4,22 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Imgs from '../components/Imgs';
+import Form from 'react-bootstrap/Form';
 
 const PlansPage = () => {
+ 
   return (
     <main>
       <Container>
-        <hr className='text-light mt-2'/>
+        <hr className='text-light mt-2' />
         <div class="Div-Musculacion">
           <h3 class="">"Plan Musculacion"</h3>
           <p class="opacity-50">"Desafía tus límites con nuestro plan de entrenamiento pesado y alcanza un nivel superior de fuerza y resistencia."</p>
         </div>
-        <hr  className='text-light'/>
+        <hr className='text-light' />
         <Row>
           <div className='Imagen-Principal-Musculatura'>
-          <Imgs url={'https://i.postimg.cc/s2ymgTRy/foto-blanco-negro-hombre-musculoso-usando-tiza-deportiva-antes-levantar-pesa-entrenamiento-pesas-gim.jpg'} alt={'Foto de Mancuernas'} id={'Plan-Musculacion-1'} />
+            <Imgs url={'https://i.postimg.cc/s2ymgTRy/foto-blanco-negro-hombre-musculoso-usando-tiza-deportiva-antes-levantar-pesa-entrenamiento-pesas-gim.jpg'} alt={'Foto de Mancuernas'} id={'Plan-Musculacion-1'} />
           </div>
           <Col>
             <div>
@@ -32,6 +34,50 @@ const PlansPage = () => {
           <hr className='text-light' />
           <hr className='text-light' />
         </Row>
+        <div className='Div-Reserva-Turno'>
+          <h2>° Reserva Tu Turno:</h2>
+        </div>
+        <Row>
+          <Col>
+            <div>
+              <h5 className='text-light mt-4'>Horario De La Clase:</h5>
+            </div>
+            <div>
+              <Form.Select className='Form-Hora mt-3' aria-label="Default select example">
+                <option>Seleccione el Horario en el que Desea Tener la Clase:</option>
+                <option value="1"> 8:00</option>
+                <option value="2">10:00</option>
+                <option value="3">12:00</option>
+              </Form.Select>
+            </div>
+            <Col className='Nombre-Profesor mt-4'>
+              <h2>Profesor: Diego Fernandez </h2>
+              <Col>
+              <div>
+              <Imgs url={'https://i.postimg.cc/0NHgkH24/imagen-2024-01-26-184853180.png'} alt={'Profesor de Plan de Musculacion'} id={'Profe-Plan-Musculacion'} />
+              </div>
+              </Col>
+            </Col>
+          </Col>
+        </Row>
+        {/* <div className='weather-container'>
+          <imput
+            type= 'text'
+            placeholder= 'Enter city name'
+            onChange={(e) => setCity(e.target.value)}
+          />
+          <button onClick={getWeather}>Get Weather</button>
+          {weatherInfo && (
+            <div className='weather-info'>
+              <h3></h3>
+              <p></p>
+              <p></p>
+              <p></p>
+              <p></p>
+              <p></p>
+            </div>
+          )}
+        </div> */}
       </Container>
     </main>
   )
--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -4,42 +4,42 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
 const RegisterPage = () => {
-  return (
-    <div className="login-container">
-      <div className="d-flex align-items-center justify-content-center wrap">
-        <Form className="login-form">
-          <Form.Group className="mb-3 text-white" controlId="formBasicEmail">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
-            <Form.Text className="text-muted text-white">
-              We'll never share your email with anyone else.
-            </Form.Text>
-          </Form.Group>
+    return (
+      <div className="login-container">
+        <div className="d-flex align-items-center justify-content-center wrap">
+          <Form className="login-form">
+            <Form.Group className="mb-3 text-white" controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" name='email' placeholder="Enter email" />
+              <Form.Text className="text-muted text-white">
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
 
-          <Form.Group className="mb-3 text-white" controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
-          </Form.Group>
+            <Form.Group className="mb-3 text-white" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
 
 
-          <Form.Group className="mb-3 text-white" controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
-          </Form.Group>
+            <Form.Group className="mb-3 text-white" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
 
-          <Form.Group className="mb-3 text-white" controlId="formBasicConfirmPassword">
-            <Form.Label>Confirm Password</Form.Label>
-            <Form.Control type="password" placeholder="Confirm Password" />
-          </Form.Group>
+            <Form.Group className="mb-3 text-white" controlId="formBasicConfirmPassword">
+              <Form.Label>Confirm Password</Form.Label>
+              <Form.Control type="password" placeholder="Confirm Password" />
+            </Form.Group>
 
-          <Button variant="success" type="submit">
-            Submit
-          </Button>
-        </Form>
+            <Button variant="success" type="submit">
+              Submit
+            </Button>
+          </Form>
+        </div>
       </div>
-    </div>
-  );
-};
+    )
+  }
 
 export default RegisterPage;
 

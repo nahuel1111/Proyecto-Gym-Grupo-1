fd99694948f389fe0cf7e5a9c457090c045bf37e
nahuel1111 <ricardo-nahuel@hotmail.com>
2024-02-05 22:24:03 -0300

    se hizo toda la parte del crud

--- src/pages/AdminComment.jsx
+++ src/pages/AdminComment.jsx
@@ -4,36 +4,56 @@ import { useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react'; 
 import clienteAxios, { jsonConfig } from '../helpers/axiosconfig';
 import Button from 'react-bootstrap/Button';
+import Swal from 'sweetalert2';
 
 import '../css/AdminPage.css'
 const AdminComment = () => {
   const [comment, setComment] = useState([])
+  const [update,setUpdate]= useState(false)
 
   const use = useParams()
   
+  const destacarComentario = async (comentario, event) => {
+    try {
+      event.preventDefault()
+      const nuevoDestacado = comentario.Destacado === 'false' ? 'true' : 'false'
 
-  const Destacado = async ()=>{
-    
+      const updateComment = await clienteAxios.put(`/comment/${comentario._id}`, { Destacado: nuevoDestacado }, jsonConfig)
+  if (updateComment.status === 200){
+    if(update==false){
+      setUpdate(true)
+    }else{
+      setUpdate(false)
+    }
+    Swal.fire({
+      title: "comentario actualizado con exito!",
+      icon: "success"
+    });
+      } 
+    } catch (error) {
+      console.error('Error al agregar el comentario destacado:', error)
+    }
   }
 
+
 const GetAllComments = async ()=>{
-  const Allgetcomment = await clienteAxios.get(`/users/comment/${use.id}`) 
-  setComment(Allgetcomment.data.comentario)
+  const Allgetcomment = await clienteAxios.get(`/comment/${use.id}`) 
+  setComment(Allgetcomment.data.commentUser)
 }
 
 
   useEffect(() => {
     GetAllComments()
-  },[])
+  },[update])
 
   return (
     <>
     <Container>
     <h2>Comentarios</h2>
     <div> {comment.map ((comentarios)=>
-    <p> {comentarios}    <Button variant="primary" onClick={Destacado}>
-    Destacar
-  </Button>    </p>
+    <p> {comentarios.comentario}  <Button variant="primary" onClick={(event) => destacarComentario(comentarios, event)}>
+   {comentarios.Destacado == 'true' ? 'Quitar Destacado' : 'Destacar'}
+  </Button>  </p>
     )}  </div><br />      
     </Container>
    
